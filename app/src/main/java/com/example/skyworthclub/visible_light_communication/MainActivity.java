db76fa900111bb4ec924099870bd44144f4ccae4
SkyworthClub <skyworthclub@SkyworthClubdeMacBook-Pro.local>
2017-11-25 22:18:24 +0800

    接入高德地图完成

---
+++ app/src/main/java/com/example/skyworthclub/visible_light_communication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.skyworthclub.visible_light_communication;
+
+import android.app.Activity;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends Activity {
+
+    MapView mMapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+
+        //初始化地图控制器对象
+        AMap aMap = null;
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+}
