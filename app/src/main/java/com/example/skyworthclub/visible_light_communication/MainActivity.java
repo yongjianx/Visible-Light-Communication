b800eb5a81d119406cadd5ff163deefed067cbf1
SkyworthClub <skyworthclub@SkyworthClubdeMacBook-Pro.local>
2017-11-27 18:06:13 +0800

    添加三个数据点完成

--- app/src/main/java/com/example/skyworthclub/visible_light_communication/MainActivity.java
+++ app/src/main/java/com/example/skyworthclub/visible_light_communication/MainActivity.java
@@ -18,10 +18,22 @@ import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.geocoder.GeocodeResult;
+import com.amap.api.services.geocoder.GeocodeSearch;
+import com.amap.api.services.geocoder.RegeocodeQuery;
+import com.amap.api.services.geocoder.RegeocodeResult;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -32,10 +44,13 @@ public class MainActivity extends Activity  implements LocationSource, AMapLocat
     private MapView mMapView;
     //初始化地图控制器对象
     private AMap aMap;
+    private CameraUpdate cameraUpdate;
 
     OnLocationChangedListener onLocationChangedListener;
     AMapLocationClient mlocationClient;
     AMapLocationClientOption mLocationOption;
+    //经纬度地点依次是正佳，天河城，太古汇
+    private double[] position = {23.1315797200,113.3195285800,23.1322190000,113.3226170000,23.1342510000,113.3324550000};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +65,7 @@ public class MainActivity extends Activity  implements LocationSource, AMapLocat
             aMap = mMapView.getMap();
         }
 
-        // 设置定位监听
+        // 设置定位监听，必须放在前面才能实现监听
         aMap.setLocationSource(this);
         MyLocationStyle myLocationStyle;
         //初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
@@ -58,13 +73,22 @@ public class MainActivity extends Activity  implements LocationSource, AMapLocat
         // 如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle = new MyLocationStyle();
         //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        myLocationStyle.interval(2000);
+//        myLocationStyle.interval(4000);
+
+        //连续定位、蓝点不会移动到地图中心点，定位点依照设备方向旋转，并且蓝点会跟随设备移动。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);
 
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
         aMap.setMyLocationEnabled(true);
 
-//        System.out.println("开始工作0");
+        UiSettings uiSettings = aMap.getUiSettings();
+        uiSettings.setLogoBottomMargin(-50);
+
+
+        getAdress(position[0], position[1]);
+        getAdress(position[2], position[3]);
+        getAdress(position[4],position[5]);
 
 
     }
@@ -134,6 +158,67 @@ public class MainActivity extends Activity  implements LocationSource, AMapLocat
         if(null != mlocationClient){
             mlocationClient.onDestroy();
         }
+    }
+
+    /*
+    解释指定坐标的地址
+     */
+    public void getAdress(final double x, final double y){
+        Log.e("TAG", "调用getAdress");
+
+        //地址查询器
+        GeocodeSearch geocodeSearch = new GeocodeSearch(this);
+        //设置查询参数,
+        //三个参数依次为坐标，范围多少米，坐标系
+        RegeocodeQuery regeocodeQuery = new RegeocodeQuery(new LatLonPoint(x, y), 200, GeocodeSearch.AMAP);
+        //设置查询结果监听
+        geocodeSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
+            //根据坐标获取地址信息调用
+            @Override
+            public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+                String result = regeocodeResult.getRegeocodeAddress().getFormatAddress();
+                Log.e("Shunxu","获得请求结果");
+                makepoint(x, y, result);
+            }
+            //根据地址获取坐标信息是调用
+            @Override
+            public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+            }
+        });
+        //发起异步查询请求
+        geocodeSearch.getFromLocationAsyn(regeocodeQuery);
+
+    }
+
+    public void makepoint(double x, double y, String result){
+        Log.e("Shunxu","开始绘图");
+        //北纬39.22，东经116.39，为负则表示相反方向
+        LatLng latLng=new LatLng(x, y);
+        Log.e("地址",result);
+
+        //使用默认点标记
+        Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("地点").snippet(result));
+        //改变可视区域为指定位置
+        //CameraPosition4个参数分别为位置，缩放级别，目标可视区域倾斜度，可视区域指向方向（正北逆时针算起，0-360）
+        cameraUpdate= CameraUpdateFactory.newCameraPosition(new CameraPosition(latLng,10,0,30));
+        aMap.moveCamera(cameraUpdate);//地图移向指定区域
+
+        aMap.setOnMarkerClickListener(new AMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(MainActivity.this,"点击指定位置",Toast.LENGTH_SHORT).show();
+                return false;//false 点击marker marker会移动到地图中心，true则不会
+            }
+        });
+
+        aMap.setOnInfoWindowClickListener(new AMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Toast.makeText(MainActivity.this,"点击了我的地点",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
